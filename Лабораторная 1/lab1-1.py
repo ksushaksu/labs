67fd4f48420339d13e5ea607cd0fd95429b7a1ba
Kseniya <108683887+ksushaksu@users.noreply.github.com>
2024-10-23 10:30:25 +0300

    Update lab1-1.py

--- Лабораторная 1/lab1-1.py
+++ Лабораторная 1/lab1-1.py
@@ -1,11 +1,11 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
-# TODO заменить значение пропущенного элемента средним арифметическим
+missing_index = 4
+excluded_index = 5
 
-modified_numbers = numbers[:4] + numbers[5:]
-
+modified_numbers = numbers[:missing_index] + numbers[excluded_index:]
 average_of_numbers = round((sum(modified_numbers)) / len(numbers), 2)
 
-numbers[4] = average_of_numbers
+numbers[missing_index] = average_of_numbers
 
 print("Измененный список:", numbers)
