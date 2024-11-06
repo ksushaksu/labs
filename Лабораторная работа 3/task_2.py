e1e7a10c8d358c7a1520804b6dd188d98d832bd3
Kseniya <108683887+ksushaksu@users.noreply.github.com>
2024-11-06 11:14:52 +0300

    Update task_2.py

--- Лабораторная работа 3/task_2.py
+++ Лабораторная работа 3/task_2.py
@@ -1,4 +1,3 @@
-# TODO Напишите функцию find_common_participants
 def find_common_participants(group1, group2, separator=','):
     participants1 = set(group1.split(separator))
     participants2 = set(group2.split(separator))
@@ -9,5 +8,5 @@ def find_common_participants(group1, group2, separator=','):
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
 
-common_participants = find_common_participants(participants_first_group, participants_second_group)
-print(common_participants)
+common_participants = find_common_participants(participants_first_group, participants_second_group, separator='|')
+print("Общие участники:", common_participants)
